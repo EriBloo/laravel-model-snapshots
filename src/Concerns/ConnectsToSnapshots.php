ef8d6461e9c25471353e782bc92de8cc8782f080
EriBloo <lprzybyslawski@gmail.com>
2023-01-25 21:41:56 +0100

    wip

---
+++ src/Concerns/ConnectsToSnapshots.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EriBloo\LaravelModelSnapshots\Concerns;
+
+use EriBloo\LaravelModelSnapshots\Models\Relations\MorphSnapshotModels;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+/**
+ * @mixin Model
+ */
+trait ConnectsToSnapshots
+{
+    /**
+     * @param  class-string  $snapshotClass
+     * @return MorphSnapshotModels
+     */
+    public function morphSnapshotModels(string $snapshotClass): MorphSnapshotModels
+    {
+        return new MorphSnapshotModels($snapshotClass, $this);
+    }
+
+    /**
+     * @param  class-string  $snapshotClass
+     * @return MorphToMany
+     */
+    public function morphSnapshots(string $snapshotClass): MorphToMany
+    {
+        return $this->morphToMany(config('model-snapshots.snapshot_class'), 'model', 'model_snapshots_relations')
+            ->where('model_snapshots.model_type', $snapshotClass);
+    }
+}
