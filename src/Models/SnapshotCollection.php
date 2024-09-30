fab7419b5be2c57f26595a559382852a114e9a8d
EriBloo <19932449+EriBloo@users.noreply.github.com>
2024-09-30 21:18:29 +0000

    chore: phpstan level 5

--- src/Models/SnapshotCollection.php
+++ src/Models/SnapshotCollection.php
@@ -7,15 +7,18 @@ namespace EriBloo\LaravelModelSnapshots\Models;
 use EriBloo\LaravelModelSnapshots\Contracts\Snapshot as SnapshotContract;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * @extends Collection<int, Snapshot>
+ */
 class SnapshotCollection extends Collection
 {
-    public function toModels(bool $fillExcludedAttributes = false): SnapshotCollection
+    public function toModels(bool $fillExcludedAttributes = false): Collection
     {
         if ($fillExcludedAttributes) {
             $this->loadMissing('subject');
         }
 
-        return new SnapshotCollection(
+        return new Collection(
             $this->map(
                 fn (SnapshotContract $snapshot) => $snapshot->toModel($fillExcludedAttributes)
             )
